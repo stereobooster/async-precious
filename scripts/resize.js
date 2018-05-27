fdb9e2c39808ffcf9a8dce9608dc2cdba57d7210
stereobooster <stereobooster@gmail.com>
2018-05-27 22:16:15 +0200

    data for srcset

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,34 +1,88 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const path = require('path')
 const sharp = require('sharp')
+const crypto = require('crypto')
+const hash = crypto.createHash('sha512')
 
-const widths = [360, 750, 1440, 1920]
+const widths = [360, 750, 1440, 1920].reverse()
 
-const fileWithWidth = (file, width, extension = '$1') =>
-  file.replace(/\.(jpe?g|png)$/i, `-${width}.${extension}`)
+const fileName = ({file, width, extension, digest}) => {
+  extension = extension || '$1'
+  digest = digest ? `.${digest}` : ''
+  width = width ? `-${width}` : ''
+  return file.replace(/\.(jpe?g|png)$/i, `${width}${digest}.${extension}`)
+}
 
-const resize = file => {
-  const original = sharp(file)
-  const resizedFiles = []
-  const originalWebp = new Promise((resolve, reject) => {
-    original.toBuffer((err, data, info) => {
+const getDigest = data =>
+  hash
+    .update(data)
+    .digest('base64')
+    .replace(/[\/\+=]/g, '')
+    .substring(0, 10)
+
+const getLqip = (sharpInstance, format) =>
+  sharpInstance
+    .resize(20)
+    .toBuffer()
+    .then(data => {
+      return `data:image/${format};base64,${data.toString('base64')}`
+    })
+
+const getInfo = sharpInstance =>
+  new Promise((resolve, reject) => {
+    sharpInstance.toBuffer(async (err, data, info) => {
       if (err) return reject(err)
-      fs
-        .createReadStream(file)
-        .pipe(fs.createWriteStream(fileWithWidth(file, info.width)))
-      original
-        .toFile(fileWithWidth(file, info.width, 'webp'))
-        .then(resolve, reject)
+      const {width, format, height, size} = info
+      const digest = getDigest(data)
+      return resolve({width, height, size, digest, format})
     })
   })
-  resizedFiles.push(originalWebp)
-  widths.forEach(width => {
+
+const writeFile = ({filename, data}) =>
+  new Promise((resolve, reject) =>
+    fs.writeFile(
+      filename,
+      data,
+      'UTF-8',
+      err => (err ? reject(err) : resolve()),
+    ),
+  )
+
+const resize = async file => {
+  const original = sharp(file)
+  const info = await getInfo(original)
+  const {width, digest, format} = info
+  const resizedFiles = []
+  const availableWidths = widths.filter(x => x < width)
+  availableWidths.unshift(width)
+  availableWidths.forEach(width => {
     const resized = original.resize(width)
-    resizedFiles.push(resized.toFile(fileWithWidth(file, width)))
-    resizedFiles.push(resized.toFile(fileWithWidth(file, width, 'webp')))
+    resizedFiles.push(resized.toFile(fileName({file, width, digest})))
+    resizedFiles.push(
+      resized.toFile(fileName({file, width, extension: 'webp', digest})),
+    )
   })
-  return Promise.all(resizedFiles)
+  let resizedInfos = await Promise.all(resizedFiles)
+  resizedInfos = resizedInfos.map(({format, width, size}) => ({
+    format,
+    width,
+    size,
+  }))
+  // info
+  const lqip = await getLqip(sharp(file), format)
+  const {name, ext} = path.parse(file)
+  return writeFile({
+    filename: fileName({file, extension: 'json'}),
+    data: JSON.stringify({
+      ...info,
+      lqip,
+      name,
+      ext,
+      sizes: resizedInfos,
+    }),
+  })
 }
 
 resize(process.argv[2])
--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react'
-import rpi from 'rpi.macro'
-import size from 'filesize.macro'
-
-import imageJ1 from './images/andre-spieker-238-unsplash.jpg'
-import imageJ2 from './images/jairo-alzate-45522-unsplash.jpg'
-import imageJ3 from './images/vincent-van-zalinge-408523-unsplash.jpg'
-import imageJ4 from './images/marvin-meyer-188676-unsplash.jpg'
-import imageJ5 from './images/nidhin-mundackal-281287-unsplash.jpg'
-
-import imageW1 from './images/andre-spieker-238-unsplash.webp'
-import imageW2 from './images/jairo-alzate-45522-unsplash.webp'
-import imageW3 from './images/vincent-van-zalinge-408523-unsplash.webp'
-import imageW4 from './images/marvin-meyer-188676-unsplash.webp'
-import imageW5 from './images/nidhin-mundackal-281287-unsplash.webp'
-
 // import {AdaptiveLoad} from 'react-precious-image'
 import AdaptiveLoad from './components/AdaptiveLoad'
 import icons from './components/icons'
 import theme from './components/theme.module.css'
+
+import image1 from './images/andre-spieker-238-unsplash.json'
+import image2 from './images/jairo-alzate-45522-unsplash.json'
+import image3 from './images/vincent-van-zalinge-408523-unsplash.json'
+import image4 from './images/marvin-meyer-188676-unsplash.json'
+import image5 from './images/nidhin-mundackal-281287-unsplash.json'
 
 const Al = props => <AdaptiveLoad {...props} />
 
@@ -28,55 +19,23 @@ Al.defaultProps = {
   threshold: 1000,
 }
 
-const meta1 = rpi('./images/andre-spieker-238-unsplash.jpg')
-const meta2 = rpi('./images/jairo-alzate-45522-unsplash.jpg')
-const meta3 = rpi('./images/vincent-van-zalinge-408523-unsplash.jpg')
-const meta4 = rpi('./images/marvin-meyer-188676-unsplash.jpg')
-const meta5 = rpi('./images/nidhin-mundackal-281287-unsplash.jpg')
+const src = ({name, ext, digest, width}) => `/images/${name}-${width}.${digest}.${ext}`
 
-const size1 = size('./images/andre-spieker-238-unsplash.webp')
-const size2 = size('./images/jairo-alzate-45522-unsplash.webp')
-const size3 = size('./images/vincent-van-zalinge-408523-unsplash.webp')
-const size4 = size('./images/marvin-meyer-188676-unsplash.webp')
-const size5 = size('./images/nidhin-mundackal-281287-unsplash.webp')
+const props = obj => ({
+  width: obj.width,
+  height: obj.height,
+  src: src(obj),
+  placeholder: {lqip: obj.lqip},
+  webp: src({...obj, ext: 'webp'}),
+})
 
 const App = () => (
   <React.Fragment>
-    <Al
-      {...meta1}
-      src={imageJ1}
-      alt="doggo 1"
-      webp={imageW1}
-      webpSize={size1}
-    />
-    <Al
-      {...meta2}
-      src={imageJ2}
-      alt="doggo 2"
-      webp={imageW2}
-      webpSize={size2}
-    />
-    <Al
-      {...meta3}
-      src={imageJ3}
-      alt="doggo 3"
-      webp={imageW3}
-      webpSize={size3}
-    />
-    <Al
-      {...meta4}
-      src={imageJ4}
-      alt="doggo 4"
-      webp={imageW4}
-      webpSize={size4}
-    />
-    <Al
-      {...meta5}
-      src={imageJ5}
-      alt="doggo 5"
-      webp={imageW5}
-      webpSize={size5}
-    />
+    <Al alt="doggo 1" {...props(image1)} />
+    <Al alt="doggo 2" {...props(image2)} />
+    <Al alt="doggo 3" {...props(image3)} />
+    <Al alt="doggo 4" {...props(image2)} />
+    <Al alt="doggo 5" {...props(image5)} />
   </React.Fragment>
 )
 
